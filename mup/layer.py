242bb8f7381ea65575dffa0de9cea9324c5534a8
JeremyCCHsu <jeremycchsu@gmail.com>
2024-02-27 05:43:12 +0000

    1.0.2: add MuReadout counterparts for Conv1d & Conv2d

--- mup/layer.py
+++ mup/layer.py
@@ -1,7 +1,7 @@
 # Copyright 2022 Microsoft Corporation.
 # Copyright 2022 Resemble AI.
 import torch
-from torch.nn import Linear
+from torch.nn import Linear, Conv1d, Conv2d
 
 
 class MuReadout(Linear):
@@ -68,6 +68,85 @@ class MuSharedReadout(MuReadout):
     def __init__(self, weight, bias=True, **kwargs):
         super().__init__(*weight.shape, bias=bias, **kwargs)
         self.weight = weight
+
+
+class MuConv1dOut(Conv1d):
+    '''Drop-in replacement for all output linear layers.
+
+    An "output" linear layer is one that maps from a width dimension (e.g.,
+    `d_model` in a Transformer) to a non-width dimension (e.g., vocab size).
+
+    This layer implements the version of μP with a 1/width multiplier and a
+    constant variance initialization for both weights and biases.
+    '''
+    def __init__(self, *args, readout_zero_init=False, output_mult=1.0, **kwargs):
+        self.output_mult = output_mult
+        self.readout_zero_init = readout_zero_init
+        super().__init__(*args, **kwargs)
+
+    def _rescale_parameters(self):
+        '''Rescale parameters to convert SP initialization to μP initialization.
+
+        Warning: This method is NOT idempotent and should be called only once
+        unless you know what you are doing.
+        '''
+        if hasattr(self, '_has_rescaled_params') and self._has_rescaled_params:
+            raise RuntimeError(
+                "`_rescale_parameters` has been called once before already. "
+                "Unless you know what you are doing, usually you should not be calling `_rescale_parameters` more than once.\n"
+                "If you called `set_base_shapes` on a model loaded from a checkpoint, "
+                "or just want to re-set the base shapes of an existing model, "
+                "make sure to set the flag `rescale_params=False`.\n"
+                "To bypass this error and *still rescale parameters*, set `self._has_rescaled_params=False` before this call.")
+        if self.bias is not None:
+            self.bias.data *= self.width_mult()**0.5
+        self.weight.data *= self.width_mult()**0.5
+        self._has_rescaled_params = True
+
+    def forward(self, x):
+        return super().forward(
+            self.output_mult * x / self.width_mult())
+
+
+
+class MuConv2dOut(Conv2d):
+    '''Drop-in replacement for all output linear layers.
+
+    An "output" linear layer is one that maps from a width dimension (e.g.,
+    `d_model` in a Transformer) to a non-width dimension (e.g., vocab size).
+
+    This layer implements the version of μP with a 1/width multiplier and a
+    constant variance initialization for both weights and biases.
+    '''
+    def __init__(self, *args, readout_zero_init=False, output_mult=1.0, **kwargs):
+        self.output_mult = output_mult
+        self.readout_zero_init = readout_zero_init
+        super().__init__(*args, **kwargs)
+
+    def _rescale_parameters(self):
+        '''Rescale parameters to convert SP initialization to μP initialization.
+
+        Warning: This method is NOT idempotent and should be called only once
+        unless you know what you are doing.
+        '''
+        if hasattr(self, '_has_rescaled_params') and self._has_rescaled_params:
+            raise RuntimeError(
+                "`_rescale_parameters` has been called once before already. "
+                "Unless you know what you are doing, usually you should not be calling `_rescale_parameters` more than once.\n"
+                "If you called `set_base_shapes` on a model loaded from a checkpoint, "
+                "or just want to re-set the base shapes of an existing model, "
+                "make sure to set the flag `rescale_params=False`.\n"
+                "To bypass this error and *still rescale parameters*, set `self._has_rescaled_params=False` before this call.")
+        if self.bias is not None:
+            self.bias.data *= self.width_mult()**0.5
+        self.weight.data *= self.width_mult()**0.5
+        self._has_rescaled_params = True
+
+    def forward(self, x):
+        return super().forward(
+            self.output_mult * x / self.width_mult())
+
+
 
 def rescale_linear_bias(linear):
     '''Rescale bias in nn.Linear layers to convert SP initialization to μP initialization.
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="mup",
-    version="1.0.1",
+    version="1.0.2",
     author="Edward J Hu, Greg Yang",
     author_email="edwardjhu@edwardjhu.com, gregyang@microsoft.com",
     description="Maximal Update Parametrization",
